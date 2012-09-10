66e71d7b67fa90b8f40a2c7bd2ee6ad4744b8d39
seven89 <fas.schaefer@gmail.com>
2012-09-10 13:22:09 +0200

    Erste Packages und Klassen initial erstellt

---
+++ BlackJack/src/KnowledgeSystem/BlackJackAlgo.java
@@ -0,0 +1,10 @@
+package KnowledgeSystem;
+/**
+ * 
+ * @author Fabian Schäfer
+ * @brief Diese Klasse implementiert den Hauptalgorithmus des KnowledgeBased Systems
+ *
+ */
+public class BlackJackAlgo {
+
+}
