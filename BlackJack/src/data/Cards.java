98276e00340b711d080b7d6cb53dffb9c539c283
ruclip <boris.beck.de@gmail.com>
2012-09-18 12:25:39 +0200

    cards

--- BlackJack/src/data/Cards.java
+++ BlackJack/src/data/Cards.java
@@ -2,9 +2,28 @@ package data;
 
 public class Cards {
 	
+	public int[][] CardDeck;
+
+	
+	
+
+	// Konstruktor
+	public void Cards(){
+		CardDeck= new int[12][3];
+		int i = 0;
+		int j = 0;
+		
+		for  (i=0; i<12; i++) {
+			CardDeck[i][j] = CardDeck[i+1][j];
+			for (j=0; j<3; j++) {
+				CardDeck[i][j] = CardDeck[i][j+1];
+			}
+		}	
+	}
+	
 	// CardTypes	
 	public enum CardTypes {
-		TWO("2"), THREE("3"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN ("7"), EIGHT("8"), NINE("9"), TEN("10"), JACK("11"), QUEEN("12"), KING("13"), ACE("14");
+		TWO("0"), THREE("1"), FOUR("2"), FIVE("3"), SIX("4"), SEVEN ("5"), EIGHT("6"), NINE("7"), TEN("8"), JACK("9"), QUEEN("10"), KING("11"), ACE("12");
 		
 		//public final int valor = ordinal() + 2;
 		public final String symbol;
@@ -28,7 +47,6 @@ public class Cards {
 	//CardDeck
 	private CardTypes cardType;
 	private CardIcons cardIcon;
-	private String[][] CardDeck;
 	
 	public void SetCards () {
 		
