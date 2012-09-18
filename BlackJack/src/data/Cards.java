f5c0d1cfa5de371a8e43ec68110fb7107a2ddd5f
ruclip <boris.beck.de@gmail.com>
2012-09-18 11:19:44 +0200

    cards

--- BlackJack/src/data/Cards.java
+++ BlackJack/src/data/Cards.java
@@ -2,16 +2,28 @@ package data;
 
 public class Cards {
 	
-		public enum CardTypes {
-		
-		
+	// CardTypes	
+	public enum CardTypes {
 		TWO("2"), THREE("3"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN ("7"), EIGHT("8"), NINE("9"), TEN("10"), JACK("11"), QUEEN("12"), KING("13"), ACE("14");
 		
-		public final int valor = ordinal() + 2;
+		//public final int valor = ordinal() + 2;
 		public final String symbol;
 		
 		CardTypes(String sym) {
 			symbol = sym;
 		}
 	}
+	
+	// CardIcons
+	public enum CardIcons {
+		HEART('H'), DIAMONDS('D'), SPADE('S'), CLUBS('C');
+		
+		public final char symbol;
+		
+		CardIcons(char c) {
+			symbol = c;		
+		}
+	}
+	
+	//CardDeck
 }
