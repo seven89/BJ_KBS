119822185cc60ede5848e8295b7d8482831c3238
ruclip <boris.beck.de@gmail.com>
2012-09-19 16:10:35 +0200

    cards

--- BlackJack/src/data/Cards.java
+++ BlackJack/src/data/Cards.java
@@ -3,55 +3,39 @@ package data;
 public class Cards {
 	
 	public int[][] CardDeck;
-
 	
+	//constructor
+	public Cards() {
+		CardDeck= new int[5][16];
+	}
 	
-
-	// Konstruktor
-	public void Cards(){
-		CardDeck= new int[12][3];
-		int i = 0;
-		int j = 0;
-		
-		for  (i=0; i<12; i++) {
-			CardDeck[i][j] = CardDeck[i+1][j];
-			for (j=0; j<3; j++) {
-				CardDeck[i][j] = CardDeck[i][j+1];
+	public void newCardDeck() {									// 52 Cards
+		for  (int i=1; i<4; i++) {
+			for (int j=2; j<15; j++) {
+				CardDeck[i][j] = CardDeck[i][1];				// 1=Karte im Stapel, 0= Karte nicht mehr im Stapel
+				System.out.println("CARD: " + CardDeck[i][3]);
 			}
 		}	
 	}
 	
-	// CardTypes	
-	public enum CardTypes {
-		TWO("0"), THREE("1"), FOUR("2"), FIVE("3"), SIX("4"), SEVEN ("5"), EIGHT("6"), NINE("7"), TEN("8"), JACK("9"), QUEEN("10"), KING("11"), ACE("12");
+	//getCard
+	public int[][] getCard() {
+		//Random random = new Random();
+		return CardDeck;
 		
-		//public final int valor = ordinal() + 2;
-		public final String symbol;
-		
-		CardTypes(String sym) {
-			symbol = sym;
-		}
 	}
 	
-	// CardIcons
-	public enum CardIcons {
-		HEART('H'), DIAMONDS('D'), SPADE('S'), CLUBS('C');
+	// shuffle CardDeck
+	public int[][]shuffleCardDeck() {
+		return CardDeck;
 		
-		public final char symbol;
-		
-		CardIcons(char c) {
-			symbol = c;		
-		}
 	}
 	
-	//CardDeck
-	private CardTypes cardType;
-	private CardIcons cardIcon;
 	
-	public void SetCards () {
-		
-		
-		
-		
+	// only for test
+	public static void main(String [ ] args)
+	{
+		Cards CardTest = new Cards();
+		CardTest.newCardDeck();
 	}
 }
