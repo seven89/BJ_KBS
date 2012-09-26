bcaadaa0dd4faecca88bcec177867be7c1ca890b
hagenduk <justus-m@hotmail.de>
2012-09-26 10:52:27 +0200

    punkte

--- BlackJack/src/data/CardSet.java
+++ BlackJack/src/data/CardSet.java
@@ -75,20 +75,20 @@ public class CardSet {
 	 */
 	private int getCardScore(int score) {
         switch (score) {
-        	case 0: if(highAss) { score=11; }
+        	case 1: if(highAss) { score=11; }
    		 			else{ score=1;} break;
-            case 1:  score=2; break;
-            case 2:  score=3; break;
-            case 3:  score=4; break;
-            case 4:  score=5; break;
-            case 5:  score=6; break;
-            case 6:  score=7; break;
-            case 7:  score=8; break;
-            case 8:  score=9; break;
-            case 9:  score=10; break;
+            case 2:  score=2; break;
+            case 3:  score=3; break;
+            case 4:  score=4; break;
+            case 5:  score=5; break;
+            case 6:  score=6; break;
+            case 7:  score=7; break;
+            case 8:  score=8; break;
+            case 9:  score=9; break;
             case 10: score=10; break;
             case 11: score=10; break;
             case 12: score=10; break;
+            case 13: score=10; break;
             
         }     
 		return score;
