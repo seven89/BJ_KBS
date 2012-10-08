6efaeb29c27895e9821f2de0c7416cb741cb4a8a
seven89 <fas.schaefer@gmail.com>
2012-10-08 13:06:20 +0200

    fas: Update Agent - Wahrscheinlichkeitsberechnung

--- BlackJack/src/KnowledgeSystem/Agent.java
+++ BlackJack/src/KnowledgeSystem/Agent.java
@@ -2,26 +2,28 @@
  * 
  * @author Fabian Schäfer
  * @brief This class implements the algorithm of the agent
- *
+ * @param probability: the addresses 0 till 10 are reserved for the each distinct card 
+ * value. The last address is reserved for the total number of all cards.
  */
 package KnowledgeSystem;
 
-import data.CardSet;
 import data.Rules;
 
 public class Agent extends Player{
 
-	private double [] probability;
+	private float [] probability;
+	private int [] probCounter;
 	
 	public Agent (int credit)
 	{
 		super(credit);
-		probability = new double [11];
+		probability = new float [11];
+		probCounter = new int [11];
+		initializeProbability();
 	}
 
 	//TODO: Wahrscheinlichkeit berechnen
-	//TODO: Strategie
-	//TODO: Entscheidung anhand von Kriterien treffen
+	//TODO: Strategie --> Entscheidung anhand von Kriterien treffen
 	
 	public double calcBetValue(Rules rules)
 	{
@@ -35,41 +37,83 @@ public class Agent extends Player{
 		return betValue;
 	}
 	
-	public void initializeProbability(CardSet cardDeck)
+	private void initializeProbability()
 	{
-		//TODO probability berechnen
-		for(int i = 0; i < cardDeck.getLengthSym();i++) {
-			for(int j = 0; j < cardDeck.getLengthVal();j++){
-//				if(cardDeck.[i][j] == 0){
-//					
-				}
+		/**
+		 * At the beginning each card value from 1 to 9 and 11 is contained in the
+		 * card deck four times. - the card value 10 sixteen times.
+		 */
+		for(int i = 0; i < probCounter.length; i++)
+		{
+			if(i == 9)
+			{
+				//ten, jack, queen, king
+				probCounter[i] = 16;
+			}
+			else if(i==10)
+			{
+				//total count
+				probCounter[i] = 52;
+			}
+			else 
+			{
+				//1-9, probability of 1 equals 11
+				probCounter[i]= 4;
+			}
 		}
+
 	}
 
 	public void updateProbability(int [] card) {
 		
 		/**
-		 * Updates the probabilty if a new card is pulled by a participant
+		 * Updates the probability if a new card is pulled by a participant
 		 */
-		//4x 1 - 9; 16x 10; 1x 11
 		switch(card[2])
 		{
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
-			case 5: break;
-			case 6: break;
-			case 7: break;
-			case 8: break;
-			case 9: break;
-			case 10: break;
-			case 11: break;
+			case 1: probCounter[0]--;
+					break;
+			case 2: probCounter[1]--;
+					break;
+			case 3: probCounter[2]--;
+					break;
+			case 4: probCounter[3]--;
+					break;
+			case 5: probCounter[4]--;
+					break;
+			case 6: probCounter[5]--;
+					break;
+			case 7: probCounter[6]--;
+					break;
+			case 8: probCounter[7]--;
+					break;
+			case 9: probCounter[8]--;
+					break;
+			case 10: probCounter[9]--;
+					break;
+			case 11: probCounter[0]--;
+					break;
+		}
+	}
+	
+	public void setProbability()
+	{
+		for(int i = 0; i < probability.length; i++)
+		{
+			if(i == 10)
+			{
+				//in case of ace
+				probability[i] = probability[0];
+			}
+			else 
+			{
+				probability[i] = probCounter[i]/52*100;
+			}
 		}
 	}
 	
 	//getters & setters
-	public double[] getProbability() {
+	public float[] getProbability() {
 		return probability;
 	}
 }
