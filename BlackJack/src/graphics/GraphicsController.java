0633893a3229faba250d8b3809185b63188db0d3
hagenduk <justus-m@hotmail.de>
2012-11-07 15:36:13 +0100

    -20% arbeitsspeicher bedarf -10% cpu last (manchmal)

--- BlackJack/src/graphics/GraphicsController.java
+++ BlackJack/src/graphics/GraphicsController.java
@@ -1,20 +1,14 @@
 package graphics;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Frame;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.Transparency;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -25,14 +19,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JWindow;
-
-import KnowledgeSystem.Agent;
-import KnowledgeSystem.Player;
-
-import controller.BlackJack;
-import data.CardSet;
-import data.Rules;
 
 
 public class GraphicsController extends JFrame {
@@ -44,61 +30,6 @@ public class GraphicsController extends JFrame {
 	/**
 	 * @param args
 	 */
-//	private static Image texture1a;
-//	private static Image texture1b;
-//	private static Image texture1c;
-//	private static Image texture1d;
-//	private static Image texture2a;
-//	private static Image texture2b;
-//	private static Image texture2c;
-//	private static Image texture2d;
-//	private static Image texture3a;
-//	private static Image texture3b;
-//	private static Image texture3c;
-//	private static Image texture3d;
-//	private static Image texture4a;
-//	private static Image texture4b;
-//	private static Image texture4c;
-//	private static Image texture4d;
-//	private static Image texture5a;
-//	private static Image texture5b;
-//	private static Image texture5c;
-//	private static Image texture5d;
-//	private static Image texture6a;
-//	private static Image texture6b;
-//	private static Image texture6c;
-//	private static Image texture6d;
-//	private static Image texture7a;
-//	private static Image texture7b;
-//	private static Image texture7c;
-//	private static Image texture7d;
-//	private static Image texture8a;
-//	private static Image texture8b;
-//	private static Image texture8c;
-//	private static Image texture8d;
-//	private static Image texture9a;
-//	private static Image texture9b;
-//	private static Image texture9c;
-//	private static Image texture9d;
-//	private static Image texture10a;
-//	private static Image texture10b;
-//	private static Image texture10c;
-//	private static Image texture10d;
-//	private static Image texture11a;
-//	private static Image texture11b;
-//	private static Image texture11c;
-//	private static Image texture11d;
-//	private static Image texture12a;
-//	private static Image texture12b;
-//	private static Image texture12c;
-//	private static Image texture12d;
-//	private static Image texture13a;
-//	private static Image texture13b;
-//	private static Image texture13c;
-//	private static Image texture13d;
-//	private static Image textureb;
-//
-//	
 	class Figure {
 		int type=0;
 		int value=0;
@@ -110,7 +41,6 @@ public class GraphicsController extends JFrame {
 	}
 	// Array for Objects that are to be painted (z index is rising)
 	public Figure[] graphicObjects;
-	public CardPanel[] panels;
 	public JLabel[] labels;
 	public JLabel[] stats;
 	public JLabel decision;
@@ -136,29 +66,16 @@ public GraphicsController(){
 		 stats[i]=null;
 	 }
 	 
-	 graphicObjects=new Figure[52];
+	 graphicObjects=new Figure[20];
 	 for(int i=0;i<graphicObjects.length;i++){
 		 graphicObjects[i]=new Figure();
 	 }
 	 
-	 panels=new CardPanel[99];
-	 for(int i=0;i<panels.length;i++){
-		 panels[i]=null;
-	 }
 	 
 	showpercentage();
-	
-	
-	 
-
 	setSize(1024,768);
 //	initGraphics();
 	centerOnScreen(this);
-	
-	float w1=(float) 1.02;
-	JTextField text = new JTextField("Whatever");
-    text.setText("Wahrscheinlichkeit für 1: "+ w1);
-    getContentPane().add(text);
 	
 	backgroundPanel = new BackgroundPanel(this);
 	JButton closeButton = new JButton("Close");
@@ -190,14 +107,14 @@ public GraphicsController(){
 	getContentPane().add(backgroundPanel);
 	
 	}
-	   
-	boolean firstrun=true;
+	 boolean first=true;  
 	public void paint(Graphics g) {
 		super.paint(g);
 			
 				cardPanel=new CardPanel(this, graphicObjects);
 				getContentPane().add(cardPanel);
 
+		if(first){
 		decision.setLocation(460, 300);
 		//labels
 		 for(JLabel l : stats){
@@ -212,71 +129,11 @@ public GraphicsController(){
 		    }
 		    positiony=0;
 		    positionx=0;
+			first=false;
+			}
 			this.setVisible(true);
 	}
 	
-//	public static void initGraphics(){
-//		try {
-//			texture1a = ImageIO.read( new File( "images/texture1a.png" ) );
-//			texture2a = ImageIO.read( new File( "images/texture2a.png" ) );
-//			texture3a = ImageIO.read( new File( "images/texture3a.png" ) );
-//			texture4a = ImageIO.read( new File( "images/texture4a.png" ) );
-//			texture5a = ImageIO.read( new File( "images/texture5a.png" ) );
-//			texture6a = ImageIO.read( new File( "images/texture6a.png" ) );
-//			texture7a = ImageIO.read( new File( "images/texture7a.png" ) );
-//			texture8a = ImageIO.read( new File( "images/texture8a.png" ) );
-//			texture9a = ImageIO.read( new File( "images/texture9a.png" ) );
-//			texture10a = ImageIO.read( new File( "images/texture10a.png" ) );
-//			texture11a = ImageIO.read( new File( "images/texture11a.png" ) );
-//			texture12a = ImageIO.read( new File( "images/texture12a.png" ) );
-//			texture13a = ImageIO.read( new File( "images/texture13a.png" ) );
-//			texture1b = ImageIO.read( new File( "images/texture1b.png" ) );
-//			texture2b = ImageIO.read( new File( "images/texture2b.png" ) );
-//			texture3b = ImageIO.read( new File( "images/texture3b.png" ) );
-//			texture4b = ImageIO.read( new File( "images/texture4b.png" ) );
-//			texture5b = ImageIO.read( new File( "images/texture5b.png" ) );
-//			texture6b = ImageIO.read( new File( "images/texture6b.png" ) );
-//			texture7b = ImageIO.read( new File( "images/texture7b.png" ) );
-//			texture8b = ImageIO.read( new File( "images/texture8b.png" ) );
-//			texture9b = ImageIO.read( new File( "images/texture9b.png" ) );
-//			texture10b = ImageIO.read( new File( "images/texture10b.png" ) );
-//			texture11b = ImageIO.read( new File( "images/texture11b.png" ) );
-//			texture12b = ImageIO.read( new File( "images/texture12b.png" ) );
-//			texture13b = ImageIO.read( new File( "images/texture13b.png" ) );
-//			texture1c = ImageIO.read( new File( "images/texture1c.png" ) );
-//			texture2c = ImageIO.read( new File( "images/texture2c.png" ) );
-//			texture3c = ImageIO.read( new File( "images/texture3c.png" ) );
-//			texture4c = ImageIO.read( new File( "images/texture4c.png" ) );
-//			texture5c = ImageIO.read( new File( "images/texture5c.png" ) );
-//			texture6c = ImageIO.read( new File( "images/texture6c.png" ) );
-//			texture7c = ImageIO.read( new File( "images/texture7c.png" ) );
-//			texture8c = ImageIO.read( new File( "images/texture8c.png" ) );
-//			texture9c = ImageIO.read( new File( "images/texture9c.png" ) );
-//			texture10c = ImageIO.read( new File( "images/texture10c.png" ) );
-//			texture11c = ImageIO.read( new File( "images/texture11c.png" ) );
-//			texture12c = ImageIO.read( new File( "images/texture12c.png" ) );
-//			texture13c = ImageIO.read( new File( "images/texture13c.png" ) );
-//			texture1d = ImageIO.read( new File( "images/texture1d.png" ) );
-//			texture2d = ImageIO.read( new File( "images/texture2d.png" ) );
-//			texture3d = ImageIO.read( new File( "images/texture3d.png" ) );
-//			texture4d = ImageIO.read( new File( "images/texture4d.png" ) );
-//			texture5d = ImageIO.read( new File( "images/texture5d.png" ) );
-//			texture6d = ImageIO.read( new File( "images/texture6d.png" ) );
-//			texture7d = ImageIO.read( new File( "images/texture7d.png" ) );
-//			texture8d = ImageIO.read( new File( "images/texture8d.png" ) );
-//			texture9d = ImageIO.read( new File( "images/texture9d.png" ) );
-//			texture10d = ImageIO.read( new File( "images/texture10d.png" ) );
-//			texture11d = ImageIO.read( new File( "images/texture11d.png" ) );
-//			texture12d = ImageIO.read( new File( "images/texture12d.png" ) );
-//			texture13d = ImageIO.read( new File( "images/texture13d.png" ) );
-//			textureb = ImageIO.read( new File( "images/textureb.png" ) );
-//			
-//		} catch (IOException e1) {
-//			e1.printStackTrace();
-//			System.out.println("FUCKING SHIT IT DOESNT WORK!(Picture missing)");
-//		}
-//	}
-
 	private void centerOnScreen(Container c) {
 		Dimension paneSize = c.getSize();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -310,8 +167,6 @@ public GraphicsController(){
 	}
 	
 	private class CardPanel extends JPanel {
-
-
 		/**
 		 * 
 		 */
@@ -705,7 +560,6 @@ public GraphicsController(){
 	    JLabel label1;
 
 		private void showpercentage(){
-			
 			JLabel label = new JLabel("Wahrscheinlichkeiten (in Punkten):");
 			label.setFont(new Font("Serif", Font.PLAIN, 20));
 		    label.setForeground(Color.WHITE);
@@ -805,11 +659,6 @@ public GraphicsController(){
 		for(int i=0;i<graphicObjects.length;i++){
 			 graphicObjects[i]=new Figure();
 			 graphicObjects[i].drawn=0;
-		 }
-		for(int i=0;i<panels.length;i++){
-			 
-			 if(panels[i]!=null)getContentPane().remove(panels[i]);
-			 panels[i]=null;
 		 }
 		playercards=0;
 		bankcards=0;
